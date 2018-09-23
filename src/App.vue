506be6956df56b6be3460cf14700ce1ad3313499
gabrielmustiere <gmustiere@gemy.fr>
2018-09-23 16:41:53 +0200

    use v-for and template

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
-    <div>
-        <ul>
-            <li v-for="(game,i) in games" v-bind:key="i">{{ game }} - {{ i }}</li>
+    <div class="container">
+        <ul class="list-unstyled">
+            <li v-for="(game,i) in games" v-bind:key="i">Name : {{ game }} - Index : {{ i }}</li>
         </ul>
+        <hr>
+
+        <template v-for="(game,i) in games">
+            <h4 v-bind:key="i">Name : {{ game }}</h4>
+            <p v-bind:key="i">Index : {{ i }}</p>
+        </template>
     </div>
 </template>
 
