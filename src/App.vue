fa3cd54ca9a956d16267c0184aedd70931dbe388
gabrielmustiere <gmustiere@gemy.fr>
2018-09-23 16:04:35 +0200

    use v-if

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
- <div></div>
+    <div>
+        <input type="checkbox" v-model="checked">
+        English ??? {{ checked }}
+        <div v-if="checked">Hello World</div>
+        <div v-else>Bonjour le monde</div>
+    </div>
 </template>
 
 <script>
-export default {
-  data: function() {
-    return {};
-  }
-};
+  export default {
+    data: function () {
+      return {
+        checked: 'true'
+      };
+    }
+  };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
